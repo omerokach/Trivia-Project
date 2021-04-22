ea06023d94664b3f8fa1b7b2a424cd5969e74690
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-04-23 00:14:34 +0300

    dotenv configuration

--- DB/config/config.js
+++ DB/config/config.js
@@ -1,16 +1,17 @@
-{
+require('dotenv').config();
+module.exports={
   "development": {
     "username": "root",
-    "password": "A1s2d3f4as",
-    "database": "trivia-project",
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "port": "3306",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
-    "database": "database_test",
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
