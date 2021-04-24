087ede0ce8a21a9ccdcdd13108431598ea8ab036
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-04-24 15:51:30 +0300

    adding table in main that appear when user click on high scores button

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -7,6 +7,8 @@ import TriviaBoard from "../Components/TriviaBoard";
 export default function Main() {
   const [start, setStart] = useState(false);
   const [firstQuestion, setFirstQuestion] = useState({});
+  const [clickedHighScore, setClickedHighScore] = useState(false);
+  const [highScore, setHighScore] = useState([]);
   const history = useHistory();
 
   const getSavedQuestion = async () => {
@@ -24,8 +26,14 @@ export default function Main() {
   };
 
   const highScoreButton = async () => {
-    const res = await axios.get("/high_score");
-    console.log(res.data);
+    try {
+      const res = await axios.get("/high_score");
+      setClickedHighScore(true);
+      const sorted = res.data.sort((a, b) => b.score - a.score);
+      setHighScore(sorted);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -34,6 +42,28 @@ export default function Main() {
       {start ? <TriviaBoard firstQuestion={firstQuestion} /> : ""}
       <button onClick={highScoreButton}>High scores table</button>
       <button onClick={startButton}>Start</button>
+      <div className="high-score-div">
+        {clickedHighScore && (
+          <table>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>User</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {highScore.map((obj, i) => (
+                <tr key={i}>
+                  <td>{i + 1}</td>
+                  <td>{obj.name}</td>
+                  <td>{obj.score}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 }
