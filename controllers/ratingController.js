60c237e90efc44e2ad4be25018fded35a62026e0
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-04-25 12:52:45 +0300

    fixing the rating system for saved questions

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -2,48 +2,48 @@ const { ScoreRankCalculator, Question } = require("../DB/models");
 const { sequelize } = require("../DB/models/index");
 
 const calculateQuestionRating = async (questionId) => {
-  const questionArr = await ScoreRankCalculator.findAll({
+  let questionArr = await ScoreRankCalculator.findAll({
     where: { questionId: questionId },
   });
-  console.log(questionArr);
-  //   const calculatedRatingArr = questionArr.map(
-  //     (data) => data.dataValues.rating * data.dataValues.userScore
-  //   );
-  //   let sumCalculatedRating = 0;
-  //   calculatedRatingArr.forEach((data) => (sumCalculatedRating += data));
-  //   let sumPlayerVotedScore = 0;
-  //   questionArr.forEach(
-  //     (data) => (sumPlayerVotedScore += data.dataValues.userScore)
-  //   );
-  //   finalCalculatedRating = Math.floor(sumCalculatedRating / sumPlayerVotedScore);
-  //   finalCalculatedRating === 0 ? (finalCalculatedRating = 1) : null;
-  //   const update = await Question.update(
-  //     {
-  //       rating: finalCalculatedRating,
-  //       numOfVotes: sequelize.literal("num_of_votes + 1"),
-  //     },
-  //     { where: { id: questionId } }
-  //   );
+  const calculatedRatingArr = questionArr.map(
+    (data) => data.dataValues.rating * data.dataValues.userScore
+  );
+  let sumCalculatedRating = 0;
+  calculatedRatingArr.forEach((data) => (sumCalculatedRating += data));
+  let sumPlayerVotedScore = 0;
+  questionArr.forEach(
+    (data) => (sumPlayerVotedScore += data.dataValues.userScore)
+  );
+  finalCalculatedRating = Math.floor(sumCalculatedRating / sumPlayerVotedScore);
+  finalCalculatedRating === 0 ? (finalCalculatedRating = 1) : null;
+  const update = await Question.update(
+    {
+      rating: finalCalculatedRating,
+      numOfVotes: sequelize.literal("num_of_votes + 1"),
+    },
+    { where: { id: questionId } }
+  );
 };
 
 module.exports.ratings_get = async (req, res) => {};
 
 module.exports.ratings_post = async (req, res) => {
   const data = req.body;
-  console.log(data);
+  let resDataArr = [];
   try {
-    data.ratingArr.map(async (obj) => {
-      await ScoreRankCalculator.create({
+    for (const obj of data.ratingArr) {
+      const resData = await ScoreRankCalculator.create({
         userId: data.userId,
         questionId: obj.questionId,
         rating: obj.rating,
         userScore: data.userScore,
       });
-    });
+      resDataArr.push(resData.dataValues);
+    }
   } catch (error) {
     console.log(error);
   }
-  data.ratingArr.map(async (obj) => {
+  data.ratingArr.map(async (obj, i) => {
     await calculateQuestionRating(obj.questionId);
   });
 };
