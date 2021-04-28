fa7ed312a688bc5fac09b3adde17b24a04817954
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-04-28 17:23:49 +0300

    making highscore table with unique by user name

--- controllers/highScoreController.js
+++ controllers/highScoreController.js
@@ -17,8 +17,21 @@ module.exports.highScores_get = async (req, res) => {
 
 module.exports.highScores_post = async (req, res) => {
   const userScore = req.body;
+  let existUser;
   try {
-    const newUser = await HighScores.create(userScore);
+    existUser = await HighScores.findOne({
+      where: { userName: userScore.userName },
+    });
+    if (!existUser) {
+      existUser = await HighScores.create(userScore);
+    } else {
+      if (existUser.dataValues.score < userScore.score) {
+        await HighScores.update(
+          { score: userScore.score },
+          { where: { userName: userScore.userName } }
+        );
+      }
+    }
     const dbRes = await HighScores.findAll({});
     const arr = dbRes.map((obj) => obj.toJSON());
     arr.sort((a, b) => b.score - a.score);
@@ -28,8 +41,9 @@ module.exports.highScores_post = async (req, res) => {
         userIndex = i + 1;
       }
     });
-    return res.status(200).json({ userIndex, userId: newUser.dataValues.id });
+    return res.status(200).json({ userIndex, userId: existUser.dataValues.id });
   } catch (error) {
+    console.log(error.message);
     return res.status(500).json({ message: error.message });
   }
 };
